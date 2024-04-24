a12c313e035d056311948343d08b4dc053b48e24
Ghosfreak12 <Ghosfreak12@users.noreply.github.com>
2024-04-24 17:40:25 +0000

    deploy: 952b201a8e63f9383c2c641fd7e5a681b9b29770

--- static/js/180.14c6c79e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[180],{7180:(n,t,e)=>{e.r(t),e.d(t,{default:()=>i});e(5043);var c=e(579);const i=function(n){let{onSignIn:t}=n;return(0,c.jsx)("button",{onClick:t,className:"button",children:"Sign in with Google"})}}}]);
-//# sourceMappingURL=180.14c6c79e.chunk.js.map
